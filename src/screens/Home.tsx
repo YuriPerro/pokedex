baf948138b41dbef4f0e273ffa0c5adc715a40e3
Yuri Baumgartner <yuribaumgartner2@outlook.com>
2021-06-11 03:48:28 -0300

    Add initial pagination on home / add icon search

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,36 +1,67 @@
 import React, { useEffect } from "react";
-import { Platform, StyleSheet, Text, View, StatusBar, Image, FlatList } from "react-native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
 import { Theme } from "../styles/colors";
 import { Fonts } from "../styles/fonts";
+import { AntDesign } from "@expo/vector-icons";
 
 import pokeballBg from "../assets/pokeball-icon.png";
 import { api, getPokemon } from "../services/api";
 import { Load } from "../components/Load";
 import { useState } from "react";
 import CardPokemon from "../components/CardPokemon";
+import { TouchableOpacity } from "../common/utils";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
-  const [filteredPokemons, setFilteredPokemons] = useState<any>();
+  const [filteredPokemons, setFilteredPokemons] = useState<any[]>([]);
+  const [pokemons, setPokemons] = useState<any[]>([]);
+
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
-    async function getData() {
-      const resp = await api.get("pokemon?limit=15&offset=0");
-      const results = resp.data.results;
-      const _pokemons = await Promise.all(
-        results.map(async (pokemon: any) => {
-          let pokemonFetched = await (await getPokemon(pokemon)).data;
-          return pokemonFetched;
-        })
-      );
+    fetchPokemons();
+  }, []);
 
+  const fetchPokemons = async () => {
+    const resp = await api.get(`pokemon?limit=10&offset=${page}`);
+    const results = resp.data.results;
+    const _pokemons = await Promise.all(
+      results.map(async (pokemon: any) => {
+        let pokemonFetched = await (await getPokemon(pokemon)).data;
+        return pokemonFetched;
+      })
+    );
+
+    if (page > 0) {
+      setPokemons((oldValue) => [...oldValue, ..._pokemons]);
+      setFilteredPokemons((oldValue) => [...oldValue, ..._pokemons]);
+    } else {
+      setPokemons(_pokemons);
       setFilteredPokemons(_pokemons);
-      setLoading(false);
     }
 
-    getData();
-  }, []);
+    setLoadingMore(false);
+    setLoading(false);
+  };
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1) return;
+
+    setLoadingMore(true);
+    setPage((oldValue) => oldValue + 10);
+    fetchPokemons();
+  }
 
   if (loading) return <Load />;
 
@@ -51,7 +82,25 @@ const Home = () => {
             showsVerticalScrollIndicator={false}
             keyExtractor={(item) => String(item.id)}
             renderItem={({ item, index }) => <CardPokemon key={index} pokemon={item} />}
+            onEndReachedThreshold={0.2}
+            onEndReached={({ distanceFromEnd }) => {
+              handleFetchMore(distanceFromEnd);
+            }}
+            ListFooterComponent={
+              loadingMore ? (
+                <ActivityIndicator style={{ marginVertical: 20 }} color={Theme.FIRE} size="large" />
+              ) : (
+                <></>
+              )
+            }
           />
+        </View>
+        <View style={styles.containerSearch}>
+          <TouchableOpacity
+            style={{ width: 60, height: 60, justifyContent: "center", alignItems: "center" }}
+          >
+            <AntDesign name="search1" size={24} color="white" />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -67,12 +116,12 @@ const styles = StyleSheet.create({
     marginTop: Platform.OS === "ios" ? getStatusBarHeight() : StatusBar.currentHeight,
     paddingTop: 40,
     paddingHorizontal: 10,
-    flex: 1
+    flex: 1,
   },
   title: {
     fontFamily: Fonts.Pop600,
     fontSize: 26,
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   imageBg: {
     width: 310,
@@ -87,6 +136,25 @@ const styles = StyleSheet.create({
   },
   containerFlatlist: {
     flex: 1,
+  },
+  containerSearch: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Theme.ELETRIC,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2.9,
+    elevation: 2,
+    position: "absolute",
+    bottom: Platform.OS === "ios" ? getBottomSpace() : 25,
+    right: 10,
   },
 });
 
